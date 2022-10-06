a83f7020731badf84f52864963a14e6e71049084
김성은 <kse940815@creverse.com>
2022-10-07 00:50:42 +0900

    포스트 목록, 상세 페이지에서 작성자 정보 출 력하기

--- blog/views.py
+++ blog/views.py
@@ -7,25 +7,3 @@ class PostList(ListView):
 
 class PostDetail(DetailView):
     model = Post
-
-# def index(request):
-#     posts = Post.objects.all().order_by('-pk')
-#
-#     return render(
-#         request,
-#         'blog/post_list.html',
-#         {
-#             'posts': posts,
-#         }
-#     )
-#
-# def single_post_page(request, pk):
-#     post = Post.objects.get(pk=pk)
-#
-#     return render(
-#         request,
-#         'blog/post_detail.html',
-#         {
-#             'post': post,
-#         }
-#     )
--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m8acimqyn#u5ruo89_9d@c*hmq!#qtia#mnfoxp(^g^vd^2tp='
+SECRET_KEY = 'django-insecure-z1s1qbc(ha2t%fwinv7)-#xqppp3ri&au0$(6#8z-&ebd-@_lp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -118,6 +119,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
